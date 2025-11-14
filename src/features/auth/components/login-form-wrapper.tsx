cbe96697aacc56f9c495fd412da2ed664264ed78
Shailesh Bisht <shaileshsinghbisht0411@gmail.com>
2025-11-14 14:21:07 +0530

    login handler

--- src/features/auth/components/login-form-wrapper.tsx
+++ src/features/auth/components/login-form-wrapper.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { LoginForm } from '@/features/auth/components/login-form';
+import { LoginForm } from "@/features/auth/components/login-form";
 
 const LoginFormWrapper = () => {
   return <LoginForm />;
